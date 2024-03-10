22d713aaf77be8488dbaf83c2c7af98aeeec4691
aidilbaihaqi <aidillmusirjun@gmail.com>
2024-03-10 14:21:03 +0700

    javascript string

--- javascript-dasar/script.js
+++ javascript-dasar/script.js
@@ -1,3 +1,14 @@
 // NaN (Not a Number)
-let name = 12
-typeof(name)
+console.log(typeof(NaN))
+
+// String
+let name = "Aidil Baihaqi"
+console.log(typeof(name))
+
+// String Index and Length
+console.log(name[1])
+console.log(name.length) 
+
+// String Methods 
+console.log(name.toUpperCase())
+console.log(' hello world'.trim().toUpperCase())
